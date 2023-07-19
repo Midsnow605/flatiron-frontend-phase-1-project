70c15fc711c8e81a72064bfd93f88cae02e691d7
Jose <gutierrez.jose24@gmail.com>
2023-07-19 17:12:16 -0500

    added x delete

--- main.js
+++ main.js
@@ -1,67 +1,95 @@
-// Function to fetch data from the server and update the items array
-function fetchData() {
-    fetch('http://localhost:3000/items')
-      .then((response) => response.json())
-      .then((data) => {
-        items = data;
-        displayItems();
-      })
-      .catch((error) => {
-        console.log('Error fetching data:', error);
-      });
-  }
-  
-  // Function to send a new item to the server for storage
-  function addItemToServer(item) {
-    fetch('http://localhost:3000/items', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(item)
-    })
-      .then(() => {
-        console.log('Item added successfully!');
-        fetchData(); // Fetch updated data after adding the item
-      })
-      .catch((error) => {
-        console.log('Error adding item:', error);
-      });
-  }
-  
-  // Function to update an existing item on the server
-  function updateItemOnServer(itemId, updatedItem) {
-    fetch(`http://localhost:3000/items/${itemId}`, {
-      method: 'PUT', // Use 'PUT' or 'PATCH' depending on your server's API design
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(updatedItem)
-    })
-      .then(() => {
-        console.log('Item updated successfully!');
-        fetchData(); // Fetch updated data after updating the item
-      })
-      .catch((error) => {
-        console.log('Error updating item:', error);
-      });
-  }
-  
-  // Function to delete an item from the server
-  function deleteItemFromServer(itemId) {
-    fetch(`http://localhost:3000/items/${itemId}`, {
-      method: 'DELETE',
-    })
-      .then(() => {
-        console.log('Item deleted successfully!');
-        fetchData(); // Fetch updated data after deleting the item
-      })
-      .catch((error) => {
-        console.log('Error deleting item:', error);
-      });
-  }  
 // Initialize an empty array to store the submitted items
 let items = [];
+
+// Function to fetch data from the server and update the items array
+function fetchData() {
+  fetch('http://localhost:3000/items')
+    .then((response) => response.json())
+    .then((data) => {
+      items = data;
+      displayItems();
+    })
+    .catch((error) => {
+      console.log('Error fetching data:', error);
+    });
+}
+
+// Function to send a new item to the server for storage
+function addItemToServer(item) {
+  fetch('http://localhost:3000/items', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(() => {
+      console.log('Item added successfully!');
+      fetchData(); // Fetch updated data after adding the item
+    })
+    .catch((error) => {
+      console.log('Error adding item:', error);
+    });
+}
+
+// Function to delete an item from the server
+function deleteItemFromServer(itemId) {
+  fetch(`http://localhost:3000/items/${itemId}`, {
+    method: 'DELETE',
+  })
+    .then(() => {
+      console.log('Item deleted successfully!');
+      fetchData(); // Fetch updated data after deleting the item
+    })
+    .catch((error) => {
+      console.log('Error deleting item:', error);
+    });
+}
+
+// Function to create the "X" button for an item
+function createDeleteButton(itemId) {
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button');
+  deleteButton.textContent = 'X';
+
+  // Attach a click event listener to the delete button
+  deleteButton.addEventListener('click', function() {
+    deleteItemFromServer(itemId); // Remove item from the server
+  });
+
+  return deleteButton;
+}
+
+// Function to display the items on the DOM
+function displayItems() {
+  const itemListDiv = document.getElementById('itemList');
+  itemListDiv.innerHTML = ''; // Clear the previous content
+
+  // Loop through the items array and create HTML elements for each item
+  items.forEach(function(item) {
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('item');
+
+    const itemImage = document.createElement('img');
+    itemImage.src = item.image;
+    itemImage.alt = item.name;
+    itemDiv.appendChild(itemImage);
+
+    const itemName = document.createElement('h3');
+    itemName.textContent = item.name;
+    itemDiv.appendChild(itemName);
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = item.description;
+    itemDiv.appendChild(itemDescription);
+
+    // Create the "X" button for this item
+    const deleteButton = createDeleteButton(item.id);
+    itemDiv.appendChild(deleteButton);
+
+    itemListDiv.appendChild(itemDiv);
+  });
+}
 
 // Function to handle form submission
 function addItem(event) {
@@ -94,40 +122,6 @@ function addItem(event) {
   document.getElementById('categoryInput').value = '';
 }
 
-  // Update the DOM to display the items
-  displayItems();
-
-// Function to display the items on the DOM
-function displayItems() {
-  const itemListDiv = document.getElementById('itemList');
-  itemListDiv.innerHTML = ''; // Clear the previous content
-
-  // Loop through the items array and create HTML elements for each item
-  items.forEach(function(item) {
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('item');
-
-    const itemImage = document.createElement('img');
-    itemImage.src = item.image;
-    itemImage.alt = item.name;
-    itemDiv.appendChild(itemImage);
-
-    const itemName = document.createElement('h3');
-    itemName.textContent = item.name;
-    itemDiv.appendChild(itemName);
-
-    const itemDescription = document.createElement('p');
-    itemDescription.textContent = item.description;
-    itemDiv.appendChild(itemDescription);
-
-    itemListDiv.appendChild(itemDiv);
-  });
-}
-
-// Event listener for form submission
-const addItemForm = document.getElementById('addItemForm');
-addItemForm.addEventListener('submit', addItem);
-
 // Calculate total value function
 function calculateTotalValue() {
   let totalValue = items.reduce(function(acc, item) {
@@ -137,6 +131,13 @@ function calculateTotalValue() {
   document.getElementById('totalValue').textContent = 'Total Value: $' + totalValue.toFixed(2);
 }
 
+// Event listener for form submission
+const addItemForm = document.getElementById('addItemForm');
+addItemForm.addEventListener('submit', addItem);
+
 // Event listener for the "Calculate Total Value" button
 const calculateTotalButton = document.getElementById('calculateTotal');
 calculateTotalButton.addEventListener('click', calculateTotalValue);
+
+// Fetch initial data from the server and display items on the DOM
+fetchData();
